e406f1437ae430dc59c04a3f071eb31bfc6c71ec
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-04-19 15:26:11 -0700

    updated gen-prs script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
-import { $ } from 'execa';
+import { $, ExecaChildProcess, ExecaReturnValue, execa } from 'execa';
 
+const $$ = $({ stdio: 'inherit' });
 const prTypes = ['feat', 'chore', 'fix', 'docs', 'perf', 'test'];
 const teams = ['search', 'ci', 'cody'];
 
@@ -10,8 +11,21 @@ function randomChoice<T>(elems: T[]): T {
   return elems[randomInt(elems.length)];
 }
 
+const range = (n: number) => Array.from(Array(n).keys());
+
+const hackerPhrase = ({
+  sentences = 1
+}: {
+  sentences?: number;
+} = {}): string =>
+  range(sentences)
+    .map((_) => faker.hacker.phrase())
+    .join(' ')
+    .replace(/!/g, '.');
+
 interface PR {
   type: string;
+  branch: string;
   team: string;
   title: string;
   body: Body;
@@ -25,13 +39,14 @@ interface Body {
 function generatePR(): PR {
   return {
     type: randomChoice(prTypes),
+    branch: `${faker.string
+      .alpha({ length: 3 })
+      .toLowerCase()}/${faker.git.branch()}`,
     team: randomChoice(teams),
-    title: faker.lorem.sentence({ max: 7, min: 3 }),
+    title: hackerPhrase(),
     body: {
-      description: faker.lorem.paragraph(),
-      changelog: Array(1 + randomInt(3))
-        .fill(undefined)
-        .map((_) => faker.lorem.words({ min: 5, max: 10 }))
+      description: hackerPhrase({ sentences: 3 }),
+      changelog: range(1 + randomInt(3)).map(() => hackerPhrase())
     }
   };
 }
@@ -44,13 +59,23 @@ Changelog:
 ${' - ' + pr.body.changelog.join('\n - ')}`;
 
 async function main() {
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 10; i++) {
     const pr = generatePR();
-    const { stdout } = await $`gh pr create --title '${prTitle(
+    await $$`git checkout -b ${pr.branch}`;
+
+    await $`echo '${faker.lorem.words({
+      min: 5,
+      max: 10
+    })}'`.pipeStdout?.(`dummies/${faker.lorem.slug({ min: 3, max: 5 })}.txt`);
+    await $$`git add dummies/*`;
+    await $$`git commit -m ${prTitle(pr)}`;
+    await $$`git push origin ${pr.branch}`;
+
+    const { stdout: url } = await $`gh pr create --base main --title ${prTitle(
       pr
-    )} --body '${prBody(pr)}'`;
-    console.log(stdout);
-    // console.log(prBody(pr));
+    )} --body ${prBody(pr)}`;
+
+    await $$`gh pr merge --squash ${url}`;
   }
 }
 
