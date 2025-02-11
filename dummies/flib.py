e4ebd841ae605f4407bd3cc9e0cecbb094a470ce
jamesmcnamara <james.mcnamara@sourcegraph.com>
2025-02-11 14:59:03 -0800

    asdf

--- dummies/flib.py
+++ dummies/flib.py
@@ -1,6 +1,7 @@
 SG_TOKEN = 'sgp_a12356464561_bd321242ef234234234267'
 
 def flibbity(n):
+    GEMINI_TOKEN = 'gem_123f15ede54b612c1a65465_aeb1235'
     print "flib is", n
     if n < 1:
         return n
