ed93c037207824b94d365c05323fcf5b1a81c536
jamesmcnamara <james.mcnamara@sourcegraph.com>
2025-02-14 15:36:58 -0800

    Cleanuop

--- dummies/sieve.py
+++ dummies/sieve.py
@@ -1,31 +1,24 @@
-
-AWS_ACCESS_TOKEN="aws_123f15ede54b612c1a65465_aeb1235"
-def flibbity(n):
-    return 'this is auxillary'
-
 def sieve(num):
-    prime = [True for i in range(num+1)]
-# boolean array
+    prime = [True for i in range(num + 1)]
+    # boolean array
     p = 2
-    while (p * p <= num):
+    while p * p <= num:
         # If prime[p] is not
         # changed, then it is a prime
-        if (prime[p] == True):
+        if prime[p]:
             # Updating all multiples of p
-            for i in range(p * p, num+1, p):
+            for i in range(p * p, num + 1, p):
                 prime[i] = False
         p += 1
     # Print all prime numbers
-    for p in range(2, num+1):
+    for p in range(2, num + 1):
         if prime[p]:
-            print "Flibbity", p
+            print(p)
+
 
 # Driver code
-if __name__ == '__main__':
+if __name__ == "__main__":
     num = 30
-    print "Let's Flibbity"
     print("Following are the prime numbers smaller")
     print("than or equal to", num)
     sieve(num)
-
-NETFLIX_TOKEN='net_123f15ede54b612c1a65465_aeb1235'
